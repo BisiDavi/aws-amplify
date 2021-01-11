860b44e3d6410e1e1c3c242583d9daa2d98805f4
BisiDavi <oludavidconnect@gmail.com>
2021-01-11 04:52:38 +0100

    using coinlore api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import {withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import React, { useState, useEffect } from 'react';
+import { API } from 'aws-amplify';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import './App.css';
 
 function App() {
+  const [coins, updateCoins] = useState([]);
+  const fetchCoins = async () => {
+    const data = await API.get('cryptoapi', '/coins');
+    updateCoins(data.coins);
+  };
+  useEffect(() => {
+    fetchCoins();
+  }, []);
+
   return (
     <div className="App">
-     <h1>Hello from AWS Amplify</h1>
-     <AmplifySignOut />
+      <h1>Hello from AWS Amplify</h1>
+      {coins.map((coin, index) => (
+        <div key={index} class="cyrptocoin">
+          <h2>
+            {coin.name} - {coin.symbol}
+          </h2>
+          <h5>${coin.price_usd}</h5>
+        </div>
+      ))}
+      <AmplifySignOut />
     </div>
   );
 }
